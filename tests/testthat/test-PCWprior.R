780d80e39b89a0345c2ebf97e4e6b4ac4bf61680
vpnsctl <alexandre.impa@gmail.com>
2023-06-01 20:37:58 +0300

    Adding tests

---
+++ tests/testthat/test-PCWprior.R
@@ -0,0 +1,8 @@
+test_that("testing arc length function", {
+  theta <- seq(from=pi,to=3*pi/2,length.out = 50)
+  arclengths <- compute_partial_arc_lengths(coords)[,3]
+
+  true_arclengths <- (theta-pi)
+  error_arc <- sum((true_arclengths - arclengths)^2)
+  testthat::expect_equal(error_arc, 0, tolerance = 1e-7)
+})
